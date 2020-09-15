71e5a813d4d343abf41c00ad7afede78f7b1f372
Ryan Brecher <52445175+Live-Dev-Design@users.noreply.github.com>
2020-09-15 09:40:52 +0100

    updated script.js with 5 objects in arrays

--- js/script.js
+++ js/script.js
@@ -8,9 +8,41 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {
+    quote: "May the Force be with you.",
+    source: 'Obi-Wan Kenobi',
+    citation:'Star Wars A New Hope',
+    year: 1977
+  },
+  {
+    quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
+    source: 'John Keating',
+    citation: 'Dead Poets Society',
+    year: 1989
+  },
+  {
+    quote: "A martini. Shaken, not stirred.",
+    source: 'James Bond',
+    citation: 'Goldfinger',
+    year: 1964
+  },
+  {
+    quote: "Yippie-ki-yay, motherf—er!",
+    source: 'John McClane', 
+    citation: 'Die Hard',
+    year: 1988
+  },
+  {
+    quote: "Roads? Where we're going we don't need roads.",
+    source: 'Dr. Emmett Brown',
+    citation: 'Back to the Future',
+    year: 1985
+  }
 
+];
 
-
+console.log(quotes);
 /***
  * `getRandomQuote` function
 ***/
