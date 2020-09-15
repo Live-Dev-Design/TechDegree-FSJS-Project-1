5484d9520372175bff1e6bc2f188d7cea4ef9c0a
Ryan Brecher <52445175+Live-Dev-Design@users.noreply.github.com>
2020-09-15 14:22:57 +0100

    added functions, css and edited HTML

--- js/script.js
+++ js/script.js
@@ -11,49 +11,71 @@ project 1 - A Random Quote Generator
 const quotes = [
   {
     quote: "May the Force be with you.",
-    source: 'Obi-Wan Kenobi',
+    source: '- Obi-Wan Kenobi',
     citation:'Star Wars A New Hope',
     year: 1977
   },
   {
     quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
-    source: 'John Keating',
+    source: '- John Keating',
     citation: 'Dead Poets Society',
     year: 1989
   },
   {
     quote: "A martini. Shaken, not stirred.",
-    source: 'James Bond',
+    source: '- James Bond',
     citation: 'Goldfinger',
     year: 1964
   },
   {
     quote: "Yippie-ki-yay, motherf—er!",
-    source: 'John McClane', 
+    source: '- John McClane', 
     citation: 'Die Hard',
     year: 1988
   },
   {
     quote: "Roads? Where we're going we don't need roads.",
-    source: 'Dr. Emmett Brown',
+    source: '- Dr. Emmett Brown',
     citation: 'Back to the Future',
     year: 1985
   }
 
 ];
 
-console.log(quotes);
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
 
+  const randomNumber = Math.floor(Math.random() * 5);
+  return quotes[randomNumber];
+
+}
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  const randomQuote = getRandomQuote();
+  let quoteData = 
+  `<p class="quote">${randomQuote.quote}</p>
+   <p classs="source">${randomQuote.source}`;
+
+  if (randomQuote.citation) {
+    quoteData += `<span class="citation">${randomQuote.citation}</span>`;
+    
+  } 
+
+  if (randomQuote.year) {
+    quoteData += `<span class="year">${randomQuote.year}</span>`;
+  } 
+
+  document.getElementById('quote-box').innerHTML = quoteData;
+}
 
 
+printQuote();
 
 /***
  * click event listener for the print quote button
