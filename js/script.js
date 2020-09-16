65acd692a1681980354b1732701c50a29c6446f4
Ryan Brecher <52445175+Live-Dev-Design@users.noreply.github.com>
2020-09-16 15:36:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -60,7 +60,8 @@ printQuote();
 
 function getRandomQuote() {
   // assign a random number between 0 - 5 to a variable
-  const randomNumber = Math.floor(Math.random() * 5);
+  const randomNumber = Math.floor(Math.random() * 4);
+  console.log(randomNumber);
   return quotes[randomNumber]; // use the random number to return an object from the quotes array
 }
 
